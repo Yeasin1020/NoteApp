686f2f440a2fe92a55a344bd9eb7e629f4dd0171
Yeasin1st <ysbd111@gmail.com>
2023-12-10 19:05:26 +0600

    searchBar

--- app/components/EditNote/EditNote.js
+++ app/components/EditNote/EditNote.js
@@ -1,64 +1,29 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { View, Text, Modal, TextInput, Button } from 'react-native'
+import React from 'react'
 
-export default class EditNote extends Component {
-  render() {
-	return (
-		<>
-		<Modal visible={visible} animationType='fade'>
-			<View style={styles.container}>
-			
-				<Text style={styles.heading}>Edit Note Screen</Text>
-				<TextInput
-					style={styles.title}
-					placeholder="Enter your title"
-					value={title} onChangeText={text => handleOnChangeText(text, 'title')}
-				/>
-				<TextInput
-					style={styles.input}
-					placeholder="Enter your note"
-					value={decs} onChangeText={text => handleOnChangeText(text, 'desc')}
-					multiline={true}
-				/>
+export default function EditNote() {
+  return (
+	<>
+	 	<Modal visible={visible} animationType='fade'>
+				<View style={styles.container}>
 				
-				<Button title="Save Note" onPress={handleSubmit} />
-				
-			</View>
-		</Modal>
-		
+					<Text style={styles.heading}>Note Screen</Text>
+					<TextInput
+						style={styles.title}
+						placeholder="Enter your title"
+						value={title} onChangeText={text => handleOnChangeText(text, 'title')}
+					/>
+					<TextInput
+						style={styles.input}
+						placeholder="Enter your note"
+						value={decs} onChangeText={text => handleOnChangeText(text, 'desc')}
+						multiline={true}
+					/>
+					
+					<Button title="Save Note" onPress={handleSubmit} />
+					
+				</View>
+			</Modal> 
 	</>
-	)
-  }
+  )
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 16,
-	},
-	heading: {
-		fontSize: 24,
-		fontWeight: 'bold',
-		marginBottom: 16,
-	},
-	input: {
-		height: 100,
-		borderColor: 'gray',
-		borderWidth: 1,
-		marginBottom: 16,
-		padding: 8,
-		borderRadius: 5,
-	},
-	title: {
-		height: 50,
-		borderRadius: 5,
-		borderColor: 'gray',
-		borderWidth: 1,
-		marginBottom: 16,
-		padding: 8,
-	},
-})
-
-
-
-// rncs
--- app/components/SearchBtn/SearchBar.js
+++ app/components/SearchBtn/SearchBar.js
@@ -1,30 +1,61 @@
-import { Dimensions, StyleSheet, TextInput, View } from "react-native"
+import { Animated, Dimensions, Keyboard, StyleSheet, TextInput, TouchableWithoutFeedback, View } from "react-native"
 import colors from "../../misc/colors"
+import { useState } from "react";
 
-export default function SearchBar({user}) {
- 
-  
+export default function SearchBar({ user }) {
+	const [searchText, setSearchText] = useState('');
+	const animatedWidth = new Animated.Value(200);
+
+
+	const handleFocus = () => {
+		// Animate the width when the search bar is focused
+		Animated.timing(animatedWidth, {
+			toValue: 300,
+			duration: 300,
+			useNativeDriver: false,
+		}).start();
+	};
+
+	const handleBlur = () => {
+		// Animate the width when the search bar is blurred
+		Animated.timing(animatedWidth, {
+			toValue: 200,
+			duration: 300,
+			useNativeDriver: false,
+		}).start();
+	};
+
 	return (
-	<>
-	<View>
-	  <TextInput placeholder="Search here" style={styles.searchBar}></TextInput>
-	</View>
-	</>
-  )
+		<>
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+				<View style={styles.container}>
+					<Animated.View style={{ width: animatedWidth }}>
+						<TextInput
+							style={styles.input}
+							placeholder="Search..."
+							value={searchText}
+							onChangeText={(text) => setSearchText(text)}
+							onFocus={handleFocus}
+							onBlur={handleBlur}
+						/>
+					</Animated.View>
+				</View>
+			</TouchableWithoutFeedback>
+		</>
+	)
 }
-const width = Dimensions.get('window').width-100;
+const width = Dimensions.get('window').width - 100;
 const styles = StyleSheet.create({
-	searchBar:{
-		width,
-		position: 'relative',
-		marginLeft: 40,
-		borderWidth: 0.5,
-		borderColor: 'black',
-		padding: 1,
-		borderRadius: 50,
-		paddingLeft: 15,
-		marginTop: 20,
-		backgroundColor: '#E7E7E9'
-		
-	}
-})
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 10,
+	},
+	input: {
+		height: 40,
+		borderWidth: 1,
+		paddingLeft: 10,
+		borderRadius: 5,
+	},
+});
